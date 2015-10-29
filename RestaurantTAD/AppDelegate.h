5f861b5a40857b443f28b67c29a1fa3e9f49efca
Diana <diana@Dianas-Mac.local>
2015-10-29 20:44:49 +0300

    Initial Commit

---
+++ RestaurantTAD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RestaurantTAD
+//
+//  Created by Diana on 10/29/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RestaurantTAD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RestaurantTAD
+//
+//  Created by Diana on 10/29/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
